785b7cff55834c1e50b5b814eed428c295299004
opunko <opunko1104@gmail.com>
2022-01-14 20:51:05 +0900

    比例成分を百倍にしたらなんか上手く行った

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ DigitalOut led[]= {
 };
 
 //CalPID speed_pid(0.003,0,0.0000130,DELTA_T,DUTY_MAX);    //速度制御のPID
-CalPID speed_pid(0.00009664,0.01933,0.000001208,DELTA_T,DUTY_MAX);    //速度制御のPID
+CalPID speed_pid(0.009664,0.01933,0.000001208,DELTA_T,DUTY_MAX);    //速度制御のPID
 CalPID angle_duty_pid(0,0,0,DELTA_T,DUTY_MAX);          //
 CalPID angle_omega_pid(4,0,0.04,DELTA_T,OMEGA_MAX);//角度制御のPID
 
